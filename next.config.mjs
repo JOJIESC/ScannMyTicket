b5f3269de3f23a046f041f62802c7e1f7ce28ce7
jorgescx <j_escobart@hotmail.com>
2024-05-20 13:25:22 -0600

    login

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    // async redirects(){
-    //     //redireciconamientos automaticos
-    //     return [
-    //         {
-    //             source: '/api/users/new',
-    //             destination: '/User/Events',
-    //             permanent: true
-    //         }
-    //     ]
-    // }
-};
+const nextConfig = {};
 
 export default nextConfig;
