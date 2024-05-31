23054127531b5139af456bd7e3a249580b29728b
jorgescx <j_escobart@hotmail.com>
2024-05-31 13:52:07 -0600

    add : location property, backButton and logout funcionality at Admin

--- src/app/Organizer/AddEvent/page.tsx
+++ src/app/Organizer/AddEvent/page.tsx
@@ -6,7 +6,7 @@ function AddEvent() {
 
             {/* IMG container */}
             <div className="flex items-center justify-center w-full mb-5">
-                <label htmlFor="dropzone-file" className="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-customGray hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-200">
+                <label htmlFor="dropzone-file" className="flex flex-col items-center justify-center w-full h-52 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-customGray hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-200">
                     <div className="flex flex-col items-center justify-center pt-5 pb-6">
                         <svg className="w-8 h-8 mb-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 16">
                             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2" />
@@ -45,6 +45,10 @@ function AddEvent() {
                             <label htmlFor="horaFinal" className="block mb-2 text-sm font-medium text-gray-900">Hora final</label>
                             <input type="time" id='horaFinal' name='endTime' className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5 dark:bg-customGray" placeholder="HH-MM" required />
                         </div>
+                    </div>
+                    <div>
+                        <label htmlFor="location">Ubicación del evento:</label>
+                        <input type="text" id="location" name='location' className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5 dark:bg-customGray" placeholder="Calle 123, Ciudad, País" required />
                     </div>
 
                     <label htmlFor="message" className="block mb-2 text-sm font-medium text-gray-900">Descripción</label>
--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -62,10 +62,10 @@ function login() {
     return (
         <div className="flex flex-col md:flex-row h-screen w-auto bg-white">
             <div className="flex flex-col justify-center items-center w-full md:w-1/2 p-8">
-                <div className="w-full max-w-xs flex flex-col ml-10 gap-3">
+                <div className="w-full max-w-xs flex flex-col gap-3">
                     <h1 className="text-5xl font-bold text-black mb-8">Inicia Sesión</h1>
                 </div>
-                <form onSubmit={handleSubmit} className="w-full max-w-lg flex flex-col ml-10 gap-3">
+                <form onSubmit={handleSubmit} className="w-full max-w-lg flex flex-col gap-3">
                     <div className="flex flex-col">
                         <label htmlFor="email" className="text-black">Correo Electrónico</label>
                         <input name='email' type="email" id="email" className="border bg-customGray border-gray-300 rounded-md p-2 " onChange={handleChange} required />
@@ -84,7 +84,7 @@ function login() {
                 </p>
             </div>
             <div className="w-full md:w-1/2 flex">
-                <Image src="/img/login.png" alt="login" className="m-auto h-auto w-auto" width={600} height={600} />
+                <Image src="/img/login.png" alt="login" className="m-auto h-auto w-auto max-md:hidden" width={600} height={600} />
             </div>
         </div>
 
